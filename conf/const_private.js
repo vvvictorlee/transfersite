e3f59fa3d4db8c5d7032d85cff47f4bb2ca08072
SwapX Tech <service@swapx.org>
2020-09-04 20:04:14 +0800

    调整private的配置

--- conf/const_private.js
+++ conf/const_private.js
@@ -1,22 +1,19 @@
 
 global.HTTP_PROVIDER = "http://192.168.38.227:18045";
 
-global.CONTRACT_FACTORY = "0x1080c40b4b70d4ccf69e0833b746ac18cd94026d"; // factory
-// global.CONTRACT_REDEEM = "0x187f99700c981A03D6d82d8a5C7adbA4df9bF2c9"; // redeem
-global.CONTRACT_REDEEM = "0xF9a180b5182c816a166F218970181971E1A7de45"; // redeem
+// global.CONTRACT_FACTORY = "0x1080c40b4b70d4ccf69e0833b746ac18cd94026d"; // factory
+global.CONTRACT_FACTORY = "0xf94c890b541c5d4182de831bf0c02c808850d0fc"; // factory
+global.CONTRACT_REDEEM = "0x8aa24ecd6f743a6026342d6928226d63f57be0c4"; // redeem
 
-global.CONTRACT_SWP = "0x004032d64de1923600c9deb28a713a536eccb2a4"; // SWP
-global.CONTRACT_USDT = "0xd4956ae41ca6587aa050da6a63c425d1fc87f116"; // USDT
+global.CONTRACT_SWP = "0x004032d64de1923600c9deb28a713a536eccb2a4"; // SWP  # token6
+global.CONTRACT_USDT = "0xd4956ae41ca6587aa050da6a63c425d1fc87f116"; // USDT # token0
 
 global.BLOCK_AWARDS =  '2000000000000000000000';
 global.MAX_SUPPLY = '5760000000000000000000000'; // *1000000000000000000
 
-global.ADDRESS_COMMUNITY = '0xAdmin';   // 社区收款账户，用于获取挖矿奖励
+global.BLOCK_AWARDS_SWP =  '1000000000000000000000';
+global.MAX_SUPPLY_SWP = '2880000000000000000000000'; // *1000000000000000000
+
+global.ADDRESS_COMMUNITY = '0x9842495d6bab5cb632777ff25b5b4c1e1d595f24';   // 社区收款账户，用于获取挖矿奖励
 global.ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
 
-global.db_config = {
-    host: 'localhost',
-    user: 'root',
-    password: 'p@ssw0rd',
-    database: 'eth_data'
-};
--- conf/db_conf.js
+++ conf/db_conf.js
@@ -5,3 +5,9 @@ global.DB_CONFIG = {
     password: 'p@ssw0rd',
     database: 'eth_data'
 };
+// global.DB_CONFIG = {
+//     host: '192.168.43.208',
+//     user: 'exone',
+//     password: 'da0llarG!',
+//     database: 'eth_data'
+// };
